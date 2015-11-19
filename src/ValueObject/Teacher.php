b8a38079ef6bad1a572f959b3872fc451b08dd10
Michal Zukowski <michalz@plocman.pl>
2015-11-19 11:11:12 +0100

    Constructor refactor

--- src/ValueObject/Teacher.php
+++ src/ValueObject/Teacher.php
@@ -25,51 +25,16 @@ class Teacher
         $canScheduleClass = false,
         $isActive = true
     ) {
-        $name = trim($name);
-
-        if (!$name || strlen($name) > 50) {
-            throw new \InvalidArgumentException(
-                'Please pass valid name parameter not more than 50 characters.'
-            );
-        }
-
-        $passwordLength = strlen(trim($password));
-        if ($passwordLength < 6 || $passwordLength > 15) {
-            throw new \InvalidArgumentException(
-                'Password with Spaces, with less than 6 characters length and more than 15 characters is not allowed.'
-            );
-        }
-
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-            throw new \InvalidArgumentException('Invalid email address');
-        }
-
-        if ($phoneNumber && !preg_match('/\+\d{1,3}\ \d{9,10}/', $phoneNumber)) {
-            throw new \InvalidArgumentException(
-                'Please pass valid work phone with country code or leave blank. For example:+44 9999999999.'
-            );
-        }
-
-        if ($mobileNumber && !preg_match('/\+\d{1,3}\ \d{9,10}/', $mobileNumber)) {
-            throw new \InvalidArgumentException(
-                'Please pass valid work phone with country code or leave blank. For example:+44 9999999999.'
-            );
-        }
-
-        if ($timeZone && !in_array($timeZone, \DateTimeZone::listIdentifiers())) {
-            throw new \InvalidArgumentException('Invalid timezone');
-        }
-
-        $this->name             = $name;
-        $this->email            = $email;
-        $this->password         = $password;
-        $this->image            = $image;
-        $this->phoneNumber      = $phoneNumber;
-        $this->mobileNumber     = $mobileNumber;
-        $this->timeZone         = $timeZone;
-        $this->about            = $about;
-        $this->canScheduleClass = $canScheduleClass;
-        $this->isActive         = $isActive;
+        $this->setName($name);
+        $this->setEmail($email);
+        $this->setPassword($password);
+        $this->setImage($image);
+        $this->setPhoneNumber($phoneNumber);
+        $this->setMobileNumber($mobileNumber);
+        $this->setTimeZone($timeZone);
+        $this->setAbout($about);
+        $this->setCanScheduleclass($canScheduleClass);
+        $this->setIsActive($isActive);
     }
 
     /**
@@ -167,5 +132,120 @@ class Teacher
     public function equals(Teacher $teacher)
     {
         return $this->email === $teacher->getEmail();
+    }
+
+    /**
+     * @param string $name
+     */
+    private function setName($name)
+    {
+        $name = trim($name);
+
+        if (!$name || strlen($name) > 50) {
+            throw new \InvalidArgumentException(
+                'Please pass valid name parameter not more than 50 characters.'
+            );
+        }
+
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $email
+     */
+    private function setEmail($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            throw new \InvalidArgumentException('Invalid email address');
+        }
+
+        $this->email = $email;
+    }
+
+    /**
+     * @param string $password
+     */
+    private function setPassword($password)
+    {
+        $passwordLength = strlen(trim($password));
+        if ($passwordLength < 6 || $passwordLength > 15) {
+            throw new \InvalidArgumentException(
+                'Password with Spaces, with less than 6 characters length and more than 15 characters is not allowed.'
+            );
+        }
+
+        $this->password = $password;
+    }
+
+    /**
+     * @param string $about
+     */
+    private function setAbout($about = null)
+    {
+        $this->about = $about;
+    }
+
+    /**
+     * @param boolean $canScheduleClass
+     */
+    private function setCanScheduleClass($canScheduleClass)
+    {
+        $this->canScheduleClass = (bool)$canScheduleClass;
+    }
+
+    /**
+     * @param string $image
+     */
+    private function setImage($image = null)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @param boolean $isActive
+     */
+    private function setIsActive($isActive)
+    {
+        $this->isActive = (bool)$isActive;
+    }
+
+    /**
+     * @param string $mobileNumber
+     */
+    private function setMobileNumber($mobileNumber)
+    {
+        if ($mobileNumber && !preg_match('/\+\d{1,3}\ \d{9,10}/', $mobileNumber)) {
+            throw new \InvalidArgumentException(
+                'Please pass valid work phone with country code or leave blank. For example:+44 9999999999.'
+            );
+        }
+
+        $this->mobileNumber = $mobileNumber;
+    }
+
+    /**
+     * @param string $phoneNumber
+     */
+    private function setPhoneNumber($phoneNumber = null)
+    {
+        if ($phoneNumber && !preg_match('/\+\d{1,3}\ \d{9,10}/', $phoneNumber)) {
+            throw new \InvalidArgumentException(
+                'Please pass valid work phone with country code or leave blank. For example:+44 9999999999.'
+            );
+        }
+
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @param string $timeZone
+     */
+    private function setTimeZone($timeZone = null)
+    {
+        if ($timeZone && !in_array($timeZone, \DateTimeZone::listIdentifiers())) {
+            throw new \InvalidArgumentException('Invalid timezone');
+        }
+
+        $this->timeZone = $timeZone;
     }
 }
