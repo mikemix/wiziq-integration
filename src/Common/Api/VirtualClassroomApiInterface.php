a09fb83636901a486702143f09b2cc036d5081c6
Michal Zukowski <michalz@plocman.pl>
2015-11-24 12:04:21 +0100

    Virtual classroom API interface

---
+++ src/Common/Api/VirtualClassroomApiInterface.php
@@ -0,0 +1,28 @@
+<?php
+namespace mikemix\Wiziq\Common\Api;
+
+use mikemix\Wiziq\Entity\Classroom;
+
+interface VirtualClassroomApiInterface
+{
+    /**
+     * Schedule a class
+     *
+     * @see http://developer.wiziq.com/class/method/create
+     * Returned response is an array as follows:
+     * [
+     *    'class_id' => 15716,
+     *    'recording_url' => 'http://live.wiziq.com/aliveext/Recorded.aspx?SessionCode=pqcTxHXEgSU%3d',
+     *    'presenters' => [
+     *        ['email' => 'tsb.kid@gmail.com', 'url' => 'http://live.wiziq.com/aliveext/LoginToSession.aspx?SessionCode=Mk5wx06KmZg%3d'],
+     *        ...
+     *    ]
+     * ]
+     *
+     * @param Classroom $classroom
+     * @return array Created classroom details
+     *
+     * @throws Exception\CallException
+     */
+    public function create(Classroom $classroom);
+}
