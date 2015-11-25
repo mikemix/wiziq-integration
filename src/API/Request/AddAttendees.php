521cf1aac50a306f60205d919fa2e52ef12cb25d
Michal Zukowski <michalz@plocman.pl>
2015-11-25 20:52:28 +0100

    AddAttendee request with unit tests

---
+++ src/API/Request/AddAttendees.php
@@ -0,0 +1,39 @@
+<?php
+namespace mikemix\Wiziq\API\Request;
+
+use mikemix\Wiziq\Common\Api\RequestInterface;
+use mikemix\Wiziq\Entity\Attendees;
+
+class AddAttendees implements RequestInterface
+{
+    /** @var int */
+    private $classroomId;
+
+    /** @var Attendees */
+    private $attendees;
+
+    public function __construct($classroomId, Attendees $attendees)
+    {
+        $this->classroomId = $classroomId;
+        $this->attendees   = $attendees;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMethod()
+    {
+        return 'add_attendees';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParams()
+    {
+        return [
+            'classroom_id'  => $this->classroomId,
+            'attendee_list' => $this->attendees->toXmlString()
+        ];
+    }
+}
---
+++ tests/API/Request/AddAttendeesTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace mikemix\Wiziq\Tests\API\Request;
+
+use mikemix\Wiziq\API\Request\AddAttendees;
+use mikemix\Wiziq\Entity\Attendees;
+
+class AddAttendeesTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Attendees|\PHPUnit_Framework_MockObject_MockObject */
+    private $attendees;
+
+    /** @var AddAttendees */
+    private $request;
+
+    public function setUp()
+    {
+        $this->attendees = $this->getMockBuilder(Attendees::class)
+            ->setMethods(['toXmlString'])
+            ->getMock();
+
+        $this->request   = new AddAttendees(12345, $this->attendees);
+    }
+
+    public function testGetMethod()
+    {
+        $this->assertEquals('add_attendees', $this->request->getMethod());
+    }
+
+    public function testGetParams()
+    {
+        $this->attendees->expects($this->once())
+            ->method('toXmlString')
+            ->will($this->returnValue('xml'));
+
+        $params = [
+            'classroom_id'  => 12345,
+            'attendee_list' => 'xml',
+        ];
+
+        $this->assertEquals($params, $this->request->getParams());
+    }
+}
